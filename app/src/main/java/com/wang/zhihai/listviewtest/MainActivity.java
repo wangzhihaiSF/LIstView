df77fe88e0d5ef1d42de1de6924af3fdf0e78f95
wangzhihaiSF <1599315665@qq.com>
2018-05-05 14:18:36 +0800

    added listView

--- app/src/main/java/com/wang/zhihai/listviewtest/MainActivity.java
+++ app/src/main/java/com/wang/zhihai/listviewtest/MainActivity.java
@@ -95,8 +95,6 @@ public class MainActivity extends AppCompatActivity {
                         intent.putExtra("data", url);
                         startActivity(intent);
                         break;
-
-
                 }
             }
         });
